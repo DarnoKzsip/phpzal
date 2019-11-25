81bfa01b7224be69eb3f1fa091b94dbb44faab1d
DarnoKzsip <konradpisz@gmail.com>
2019-11-25 11:20:17 +0100

    add infosite.php

---
+++ infosite.php
@@ -0,0 +1,17 @@
+<?php
+ $Welcome_Text = "Informacja o systemie";
+ include 'toppage_tpl.php';
+ echo '<div class = "glowny">';
+ echo '<div class = "glownysrodek">';
+ ?>
+
+    
+<body class ="body">
+    <h1>Informacje o stronie</h1>
+    </div>
+    </div>
+         </div>
+         </div>
+         <?php
+         include 'bottompage_tpl.php'; ?>
+       </body>
